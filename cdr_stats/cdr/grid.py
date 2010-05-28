cf42590753592bd5af43be7a9473631ddcc6d618
Areski Belaid <areski@gmail.com>
2010-05-28 16:13:22 +0200

    merge master to chart

--- cdr_stats/cdr/grid.py
+++ cdr_stats/cdr/grid.py
@@ -3,16 +3,14 @@ from cdr.models import CDR
 from jqgrid import JqGrid
 
 
-
-
 class ExampleGrid(JqGrid):
     model = CDR # could also be a queryset
     #fields = ['id', 'first_name', 'email'] # optional 
     #url = reverse('grid_handler')
-    url = '/examplegrid'
+    url = 'http://127.0.0.1:8000/examplegrid/'
     caption = 'My First Grid' # optional
     colmodel_overrides = {
-        'id': { 'editable': False, 'width':10 },
+        'acctid': { 'editable': False, 'width':50 },
     }
-
+    
 
--- cdr_stats/cdr/views.py
+++ cdr_stats/cdr/views.py
@@ -22,14 +22,14 @@ def grid_config(request):
     grid = ExampleGrid()
     return HttpResponse(grid.get_config(), mimetype="application/json")
 
+def show_jqgrid(request):
+    return render_to_response('cdr/show_jqgrid.html', None,
+           context_instance = RequestContext(request))
 
 def show_graph(request):
     return render_to_response('cdr/show_graph.html', None,
            context_instance = RequestContext(request))
 
-def show_cdr(request):
-    return render_to_response('cdr/show_cdr.html', None,
-           context_instance = RequestContext(request))
 
 def login(request):
     return render_to_response('cdr/login.html', None,
@@ -40,74 +40,5 @@ def index(request):
            context_instance = RequestContext(request))
 
 
-
-
-## JSON Pages
-class JsonResponse(HttpResponse):
-    def __init__(self, data):
-        HttpResponse.__init__(self, content=json_encode(data), mimetype="application/json")
-
-
-def json_cdr(request):
-    import math
-
-    selected_items = CDR.objects.all()
-    page = 1
-    total_pages = 0
-    count = 0
-    
-    if request.REQUEST.has_key('nd'):
-        limit = float(request.REQUEST['rows'])
-        page = float(request.REQUEST['page'])
-        sidx = request.REQUEST['sidx']
-        sord = request.REQUEST['sord']
-        set = request.REQUEST['set']
-
-        start = limit * (page - 1)
-        end = start + limit
-
-        if sord == "desc":
-            sord_hr = "-"
-        else:
-            sord_hr = ""
-
-        #count = Volume.objects.filter(vicepartition__server__name=server_name).exclude(type__name="BK").count()
-        count = CDR.objects.all().count()
-        total_pages = math.ceil(count / limit)
-
-        #selected_items = Volume.objects.filter(vicepartition__server__name=server_name).exclude(type__name="BK").order_by("%s%s" % (sord_hr, sidx))[int(start):int(end)]
-        selected_items = CDR.objects.order_by("%s%s" % (sord_hr, sidx))[int(start):int(end)]
-
-    data = []
-    for line in selected_items:
-        data.append({'id':line.acctid, 'cell':[
-        		line.acctid,
-                line.src,
-                line.dst,
-                line.calldate,
-                line.clid,
-                line.dcontext,
-                line.channel,
-                line.dstchannel,
-                line.lastapp,
-                line.lastdata,
-                line.duration,
-                line.billsec,
-                line.disposition,
-                line.disposition,
-                line.amaflags,
-                line.accountcode,
-                line.uniqueid,
-                line.userfield,
-                line.test
-            ]
-            })
-
-    return JsonResponse({
-        "rows":data,
-        "page":page,
-        "total":total_pages,
-        "records":count,
-    })
 
 
