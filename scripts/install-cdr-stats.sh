9f3ce31249df86126179eb0905186d73662a822e
Areski Belaid <areski@gmail.com>
2011-04-09 12:12:09 +0200

    Fix in script to install CDR-Stats on Ubuntu

--- scripts/install-cdr-stats.sh
+++ scripts/install-cdr-stats.sh
@@ -93,6 +93,7 @@ echo "Install CDR-Stats..."
 mkdir /usr/share/django_app/
 cd /usr/src/
 wget --no-check-certificate https://github.com/Star2Billing/cdr-stats/tarball/$VERSION
+mv master Star2Billing-newfies-$VERSION.tar.gz
 tar xvzf Star2Billing-cdr-stats-*.tar.gz
 rm -rf Star2Billing-cdr-stats-*.tar.gz
 mv cdr-stats cdr-stats_$DATETIME
