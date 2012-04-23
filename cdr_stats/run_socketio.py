ec47d7378735abec550181a61c7b4da961aa6094
Areski Belaid <areski@gmail.com>
2012-04-23 23:14:35 +0200

    add gevent websocket script

---
+++ cdr_stats/run_socketio.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import os, sys
+
+from gevent import monkey
+monkey.patch_all()
+
+import settings
+import django.core.handlers.wsgi
+from socketio import SocketIOServer
+
+from django.core.management import setup_environ
+setup_environ(settings)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+application = django.core.handlers.wsgi.WSGIHandler()
+
+PORT = 9000
+HOSTNAME = 'localhost'
+
+if __name__ == '__main__':
+    print('Listening on http://%s:%s' % (HOSTNAME,PORT))
+    # Start up SocketIOServer, a gevent-pywsgi server which maps the url '/socket.io'
+    SocketIOServer((HOSTNAME, PORT), application, resource="socket.io").serve_forever()
