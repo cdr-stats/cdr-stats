bb596dbbdac944c031f90e58cffc7b4d2e4b3cd8
Areski Belaid <areski@gmail.com>
2012-05-30 17:30:14 +0200

    tidy code

--- cdr_stats/cdr/tasks.py
+++ cdr_stats/cdr/tasks.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 #
 # CDR-Stats License
 # http://www.cdr-stats.org
@@ -11,6 +13,7 @@
 # The Initial Developer of the Original Code is
 # Arezqui Belaid <info@star2billing.com>
 #
+
 from django.conf import settings
 from celery.task import Task, PeriodicTask
 from celery.decorators import task, periodic_task
@@ -20,27 +23,30 @@ from cdr.common_tasks import single_instance_task
 from datetime import datetime, timedelta
 import sqlite3
 
-
-LOCK_EXPIRE = 60 * 30 # Lock expires in 30 minutes
+LOCK_EXPIRE = 60 * 30  # Lock expires in 30 minutes
 
 
 class sync_cdr_pending(PeriodicTask):
     """
     A periodic task that checks for pending calls to import
     """
-    run_every = timedelta(seconds=60) # every minute
+    run_every = timedelta(seconds=60)  # every minute
 
-    @single_instance_task(key="sync_cdr_pending", timeout=LOCK_EXPIRE)
+    @single_instance_task(key='sync_cdr_pending', timeout=LOCK_EXPIRE)
     def run(self, **kwargs):
-    	logger = self.get_logger()
-        logger.info("TASK :: sync_cdr_pending")
+        logger = self.get_logger()
+        logger.info('TASK :: sync_cdr_pending')
 
-        if settings.LOCAL_SWITCH_TYPE=='asterisk':
-            if settings.ASTERISK_IMPORT_TYPE=='mysql':
-                #Import from Freeswitch Mongo
+        if settings.LOCAL_SWITCH_TYPE == 'asterisk':
+            if settings.ASTERISK_IMPORT_TYPE == 'mysql':
+
+                # Import from Freeswitch Mongo
+
                 import_cdr_asterisk_mysql()
-        elif settings.LOCAL_SWITCH_TYPE=='freeswitch':
-            #Import from Freeswitch Mongo
+        elif settings.LOCAL_SWITCH_TYPE == 'freeswitch':
+
+            # Import from Freeswitch Mongo
+
             import_cdr()
 
         return True
@@ -50,23 +56,29 @@ class get_channels_info(PeriodicTask):
     """
     A periodic task to retrieve channels info
     """
-    run_every = timedelta(seconds=1) # every minute
+    run_every = timedelta(seconds=1)  # every minute - 60 seconds
 
-    @single_instance_task(key="get_channels_info", timeout=60) #60 seconds
+    @single_instance_task(key='get_channels_info', timeout=60)
     def run(self, **kwargs):
 
-        if settings.LOCAL_SWITCH_TYPE=='freeswitch':
+        if settings.LOCAL_SWITCH_TYPE == 'freeswitch':
             logger = self.get_logger()
-            logger.info("TASK :: get_channels_info")
+            logger.info('TASK :: get_channels_info')
 
-            #Get calldate
+            # Get calldate
             now = datetime.datetime.today()
-            date_now = datetime.datetime(now.year, now.month, now.day, now.hour, now.minute, now.second, 0)
+            date_now = datetime.datetime(
+                now.year,
+                now.month,
+                now.day,
+                now.hour,
+                now.minute,
+                now.second,
+                0,
+                )
 
-            #Retrieve SwitchID
+            # Retrieve SwitchID
             switch_id = settings.LOCAL_SWITCH_ID
-            #settings.LOCAL_SWITCH_TYPE = 'freeswitch'
-
             if settings.LOCAL_SWITCH_TYPE == 'freeswitch':
                 con = False
                 try:
@@ -80,23 +92,26 @@ class get_channels_info(PeriodicTask):
                         else:
                             accountcode = row[0]
                         number_call = row[1]
-                        logger.debug("\n%s (accountcode:%s, switch_id:%d) ==> %s" % (date_now, accountcode, switch_id, str(number_call)))
+                        logger.debug('\n%s (accountcode:%s, switch_id:%d) ==> %s'
+                                 % (date_now, accountcode, switch_id,
+                                str(number_call)))
 
                         call_json = {
-                                "switch_id" : switch_id,
-                                "call_date": date_now,
-                                "numbercall": number_call,
-                                "accountcode": accountcode,
-                                }
+                            'switch_id': switch_id,
+                            'call_date': date_now,
+                            'numbercall': number_call,
+                            'accountcode': accountcode,
+                            }
                         settings.dbcon[settings.MG_CONC_CALL].insert(call_json)
-
                 except sqlite3.Error, e:
-                    logger.error("Error %s:" % e.args[0])
+
+                    logger.error('Error %s:' % e.args[0])
                 finally:
                     if con:
                         con.close()
             elif settings.LOCAL_SWITCH_TYPE == 'asterisk':
-                #TODO: Implement concurrent calls in Asterisk
-                print "Asterisk needs to be implemented"
+
+                # TODO: Implement concurrent calls in Asterisk
+                print 'Asterisk needs to be implemented'
 
             return True
