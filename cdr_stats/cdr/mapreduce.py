0948c9717cf720ef8da5da37bcc08e5c63dd2307
Shrenik Patel <shrenik181986@gmail.com>
2012-06-01 16:02:59 +0530

    removed unwanted code

--- cdr_stats/cdr/mapreduce.py
+++ cdr_stats/cdr/mapreduce.py
@@ -152,55 +152,6 @@ def mapreduce_cdr_view():
     return (map, reduce, finalfc, out)
 
 
-def mapreduce_cdr_hour_report():
-    """
-    To get the hourly analytic of cdr
-
-       * Total calls per year-month-day-hour-min
-       * Total call duration per year-month-day-hour-min
-
-    Attributes:
-
-        * ``map`` - Grouping perform on year, month, day, hour & min
-        * ``reduce`` - Calculate call count, sum of call duration based on map
-
-    Result Collection: ``aggregate_result_cdr_hour_report``
-    """
-    (map, reduce, finalfc, out) = mapreduce_default()
-
-    # Get cdr graph by hour report
-    map = mark_safe(u'''
-        function(){
-            emit( {
-                a_Year: this.start_uepoch.getFullYear(),
-                b_Month: this.start_uepoch.getMonth() + 1,
-                c_Day: this.start_uepoch.getDate(),
-                d_Hour: this.start_uepoch.getHours(),
-                e_Min: this.start_uepoch.getMinutes(),
-            },
-            {
-                calldate__count: 1, calldate:this.start_uepoch,
-                duration__sum: this.duration
-            } )
-          }''')
-    reduce = mark_safe(u'''
-        function(key,vals) {
-            var ret = {
-                calldate__count : 0,
-                duration__sum: 0,
-                calldate: '',
-            };
-            for (var i=0; i < vals.length; i++){
-                ret.calldate__count += parseInt(vals[i].calldate__count);
-                ret.duration__sum += parseInt(vals[i].duration__sum);
-                ret.calldate = vals[i].calldate;
-            }
-            return ret;
-        }''')
-    out = 'aggregate_result_cdr_hour_report'
-    return (map, reduce, False, out)
-
-
 def mapreduce_cdr_hourly_overview():
     """
     To get the overview analytic of cdr
