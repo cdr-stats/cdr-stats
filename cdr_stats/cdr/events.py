cc332dc6880999f006a1d8fd4c8a5479d69f9436
Areski Belaid <areski@gmail.com>
2012-06-01 14:01:25 +0200

    tidy

--- cdr_stats/cdr/events.py
+++ cdr_stats/cdr/events.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from django.conf import settings
 from django_socketio import events
 from django_socketio.events import on_message
@@ -5,9 +7,8 @@ from django.contrib.auth.models import User
 from datetime import datetime, timedelta
 
 from cdr.models import Switch
-#import time
-#import random
-
+# import time
+# import random
 
 import logging
 logger = logging.getLogger('cdr-stats.filelog')
@@ -25,18 +26,21 @@ def cached(ctime=3600):
     from hashlib import sha256
 
     def decr(func):
+
         def wrp(*args, **kargs):
-            key = sha256(func.func_name+repr(args)+repr(kargs)).hexdigest()
+            key = sha256(func.func_name + repr(args) + repr(kargs)).hexdigest()
             res = cache.get(key)
             if res is None:
-                res = func(*args,**kargs)
-                cache.set(key,res,ctime)
+                res = func(*args, **kargs)
+                cache.set(key, res, ctime)
             return res
+
         return wrp
+
     return decr
 
 
-@cached(300) # 5 minutes
+@cached(300)  # 5 minutes
 def get_switch_id(key_uuid):
     """
     return the switch id
@@ -48,7 +52,9 @@ def get_switch_id(key_uuid):
     return switch.id
 
 
-@cached(300) # 5 minutes
+
+
+@cached(300)  # 5 minutes
 def get_user(user_id):
     try:
         return User.objects.get(id=user_id)
@@ -61,7 +67,7 @@ def my_message_handler(request, socket, context, message):
     logger.debug('In my_message_handler')
     logger.debug(message)
     result = {}
-    result["message"] = "VoIP-Switch"
+    result['message'] = 'VoIP-Switch'
 
     key_uuid = message['voipswitch']
     switch_id = get_switch_id(key_uuid)
@@ -81,7 +87,8 @@ def my_message_handler(request, socket, context, message):
         socket.send(result)
         return True
 
-    if not user.is_superuser and (not hasattr(user, 'userprofile') or not user.userprofile.accountcode):
+    if not user.is_superuser and (not hasattr(user, 'userprofile')
+                                  or not user.userprofile.accountcode):
         logger.debug('Wrong User')
         result[message['voipswitch']] = settings.SOCKETIO_CALLNUM_DEFAULT
         socket.send(result)
@@ -89,13 +96,15 @@ def my_message_handler(request, socket, context, message):
 
     query_var = {}
     now = datetime.today()
-    start_date = datetime(now.year, now.month, now.day, now.hour, now.minute, now.second, 0)
-    end_date = datetime(now.year, now.month, now.day, now.hour, now.minute, now.second, 0) + timedelta(seconds=1)
+    start_date = datetime(now.year, now.month, now.day, now.hour, now.minute,
+                          now.second, 0)
+    end_date = datetime(now.year, now.month, now.day, now.hour, now.minute,
+                        now.second, 0) + timedelta(seconds=1)
     query_var['call_date'] = {'$gte': start_date, '$lte': end_date}
     query_var['switch_id'] = int(switch_id)
 
     if not user.is_superuser:
-        #use accountcode in filter
+        # use accountcode in filter
         query_var['accountcode'] = str(user.userprofile.accountcode)
     collectionresult = settings.DBCON[settings.MG_CONC_CALL].find(query_var)
     if not collectionresult:
@@ -110,14 +119,14 @@ def my_message_handler(request, socket, context, message):
             value = True
             break
 
-    #No record found in collection
+    # No record found in collection
     if not value:
         result[message['voipswitch']] = 0
-        #result[message['voipswitch']] = incr = random.randint(1, 500)
+        # result[message['voipswitch']] = incr = random.randint(1, 500)
         socket.send(result)
 
 
-@events.on_message(channel="^switch-")
+@events.on_message(channel='^switch-')
 def message(request, socket, context, message):
     """
     Event handler for a switch receiving a message. First validates a
@@ -125,8 +134,8 @@ def message(request, socket, context, message):
     """
     logger.debug('In message')
     logger.debug(message)
-    result = {}
     """
+    result = {}
     while True:
         time.sleep(1)
         logger.debug('In Loop')
