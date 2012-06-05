058557b8a9be7eb1210906e5c0e48add5ab41a7f
Shrenik Patel <shrenik181986@gmail.com>
2012-06-05 18:51:12 +0530

    updated import_asterik_cdr

--- cdr_stats/cdr/management/commands/sync_cdr_asterisk.py
+++ cdr_stats/cdr/management/commands/sync_cdr_asterisk.py
@@ -15,19 +15,37 @@
 #
 from django.core.management.base import BaseCommand
 from cdr.import_cdr_asterisk_mysql import import_cdr_asterisk_mysql
+from cdr.import_cdr_freeswitch_mongodb import apply_index
 
 
 class Command(BaseCommand):
     """
     Command line to import Asterisk CDR with Mysql
     """
+    args = ' apply_index '
     help = \
         '''
 Sync Asterisk with our CDR Record table
 -----------------------------------------
-USAGE : python manage.py sync_cdr_asterisk
+USAGE : python manage.py sync_cdr_asterisk 1
+        python manage.py sync_cdr_asterisk 0
 '''
 
     def handle(self, *args, **options):
 
+        if not args or len(args) != 1:
+            print self.help
+            # print >> sys.stderr
+            raise SystemExit
+
+        apply_index_var = args[0]
+        try:
+            apply_index_var = int(apply_index_var)
+        except ValueError:
+            apply_index_var = 0
+
         import_cdr_asterisk_mysql(shell=True)
+
+        # Apply index on collection
+        if apply_index_var == 1:
+            apply_index(shell=True)
