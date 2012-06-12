f622263ec0244cf3bb6c4f1019c5d807e7a48f78
Shrenik Patel <shrenik181986@gmail.com>
2012-06-12 11:32:44 +0530

    updated asterisk mgt command

--- cdr_stats/cdr/management/commands/sync_cdr_asterisk.py
+++ cdr_stats/cdr/management/commands/sync_cdr_asterisk.py
@@ -16,38 +16,32 @@
 from django.core.management.base import BaseCommand
 from cdr.import_cdr_asterisk_mysql import import_cdr_asterisk_mysql
 from cdr.import_cdr_freeswitch_mongodb import apply_index
+from optparse import make_option
 
-
-#TODO : refactor using make_option see example sync_cdr_freeswitch
 
 class Command(BaseCommand):
     """
     Command line to import Asterisk CDR with Mysql
     """
-    args = ' apply_index '
     help = \
         '''
-Sync Asterisk with our CDR Record table
------------------------------------------
-USAGE : python manage.py sync_cdr_asterisk 1
-        python manage.py sync_cdr_asterisk 0
-'''
+        Sync Asterisk with our CDR Record table
+        -----------------------------------------
+        USAGE : python manage.py sync_cdr_asterisk --apply-index\n
+        '''
+    option_list = BaseCommand.option_list + (
+        make_option('--apply-index',
+            action='store_true',
+            dest='apply-index',
+            default=False,
+            help=help),
+        )
 
     def handle(self, *args, **options):
-
-        if not args or len(args) != 1:
-            print self.help
-            # print >> sys.stderr
-            raise SystemExit
-
-        apply_index_var = args[0]
-        try:
-            apply_index_var = int(apply_index_var)
-        except ValueError:
-            apply_index_var = 0
 
         import_cdr_asterisk_mysql(shell=True)
 
         # Apply index on collection
-        if apply_index_var == 1:
+        if options['apply-index']:
+            print "\nWe are going to apply index..."
             apply_index(shell=True)
