770966c942076cce8f5d6409270960cd21866ed1
Shrenik Patel <shrenik181986@gmail.com>
2012-08-31 19:40:38 +0530

    updated tempalte tags

--- cdr_stats/cdr/tests.py
+++ cdr_stats/cdr/tests.py
@@ -32,7 +32,9 @@ from cdr.views import cdr_view, cdr_dashboard, cdr_overview,\
                       cdr_realtime, cdr_country_report, mail_report,\
                       world_map_view, index, cdr_detail
 from cdr.functions_def import get_switch_list, get_hangupcause_name,\
-                              get_hangupcause_id
+                              get_hangupcause_id, seen_unseen, seen_unseen_word
+from cdr.templatetags.cdr_extras import hangupcause_name_with_title,\
+                                        mongo_id
 from bson.objectid import ObjectId
 from datetime import datetime, timedelta
 
@@ -401,6 +403,17 @@ class CdrModelTestCase(BaseAuthenticatedClient):
         get_hangupcause_name(2)
 
         get_hangupcause_id(self.hangupcause.code)
+
+        # Template tags
+        hangupcause_name_with_title(self.hangupcause.pk)
+        value = {'_id': {'val': 1}}
+        mongo_id(value, 'val')
+
+        seen_unseen(value)
+        seen_unseen('')
+        seen_unseen_word(value)
+
+        seen_unseen_word('')
 
     def test_cdr_search_form(self):
         data = {'switch_id': 1,
