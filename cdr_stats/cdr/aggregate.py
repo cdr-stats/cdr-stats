252f6c14d0c6011fed17c20cafd7975a89030752
Shrenik Patel <shrenik181986@gmail.com>
2012-10-26 15:55:14 +0530

    added comment in aggregate.py

--- cdr_stats/cdr/aggregate.py
+++ cdr_stats/cdr/aggregate.py
@@ -1,3 +1,17 @@
+#
+# CDR-Stats License
+# http://www.cdr-stats.org
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# Copyright (C) 2011-2012 Star2Billing S.L.
+#
+# The Initial Developer of the Original Code is
+# Arezqui Belaid <info@star2billing.com>
+#
+
 def pipeline_cdr_view_daily_report(query_var):
     """
     To get the overview analytic of cdr
