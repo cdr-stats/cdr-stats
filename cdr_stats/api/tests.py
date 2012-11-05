5e4cf9f176d3e46e71577ea5b0456c3573a12051
Shrenik Patel <shrenik181986@gmail.com>
2012-11-05 16:22:23 +0530

    updated api testcases

--- cdr_stats/api/tests.py
+++ cdr_stats/api/tests.py
@@ -48,11 +48,11 @@ class ApiTestCase(BaseAuthenticatedClient):
 
     def test_cdr_daily_api(self):
         """Test Function cdr daily api"""
-        data = {"start_uepoch": "2012-02-15",
+        data = simplejson.dumps({"start_uepoch": "2012-02-15",
                 "switch_id": 1,
                 "destination_number": 3000,
                 "accountcode": 123
-        }
+        })
         response = self.client.post('/api/v1/cdr_daily_report/', data,
             content_type='application/json', **self.extra)
         self.assertEqual(response.status_code, 401)
