757c41c17639a758f07fd059833f7ba7e8fe6c9f
Areski Belaid <areski@gmail.com>
2012-12-03 18:11:04 +0100

    add mail setting in localsetting

--- install/conf/settings_local.py
+++ install/conf/settings_local.py
@@ -60,8 +60,15 @@ REDIS_DB = 0
 
 #EMAIL BACKEND
 #=============
-# Use only in Debug mode. Not in production
-#EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# Email configuration
+DEFAULT_FROM_EMAIL = 'CDR-Stats <cdr-stats@localhost.com>'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'username@gmail.com'
+EMAIL_HOST_PASSWORD = 'password'
+EMAIL_SUBJECT_PREFIX = '[CDR-Stats] '
 
 #TASTYPIE
 #========
