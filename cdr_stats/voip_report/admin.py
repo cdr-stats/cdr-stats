b374f1b0c4ef4c71917b2e074908954bb1b1093c
Shrenik Patel <shrenik181986@gmail.com>
2013-01-08 16:05:24 +0530

    fixed import

--- cdr_stats/voip_report/admin.py
+++ cdr_stats/voip_report/admin.py
@@ -7,9 +7,10 @@ from django.shortcuts import render_to_response
 from django.db.models import Sum, Avg, Count
 
 from voip_billing.tasks import VoIPbilling
+from voip_billing.forms import FileImport
 from voip_billing.function_def import check_celeryd_process
 from voip_report.models import VoIPCall_Report, VoIPCall
-from voip_report.forms import VoipSearchForm, FileImport
+from voip_report.forms import VoipSearchForm
 from voip_report.function_def import voipcall_record_common_fun, get_disposition_id,\
     get_disposition_name
 from user_profile.models import UserProfile
