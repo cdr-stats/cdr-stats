655bba5361f74c29bf63d2df779db028adbde41f
Shrenik Patel <shrenik181986@gmail.com>
2013-01-10 15:42:26 +0530

    fixed voip_rate api

--- cdr_stats/api/voip_rate_api.py
+++ cdr_stats/api/voip_rate_api.py
@@ -47,11 +47,15 @@ class VoipRateResource(ModelResource):
 
          CURL Usage::
 
-             curl -u username:password -H 'Accept: application/json' http://localhost:8000/api/v1/voip_rate/%code%/?format=json
+             curl -u username:password -H 'Accept: application/json' -X POST --data '{"code": "34"}' http://localhost:8000/api/v1/voip_rate/?format=json
 
                  or
 
              curl -u username:password -H 'Accept: application/json' http://localhost:8000/api/v1/voip_rate/?format=json
+
+                or
+
+             curl -u username:password -H 'Accept: application/json' -X POST --data '{"recipient_phone_no": "34650784355"}' http://localhost:8000/api/v1/voip_rate/?format=json
 
          Response::
 
@@ -78,11 +82,6 @@ class VoipRateResource(ModelResource):
                }
             ]
 
-    **Create**:
-
-        CURL::
-
-            curl -u username:password -H 'Accept: application/json' -X POST --data '{"recipient_phone_no": "34650784355"}' http://localhost:8000/api/v1/voip_rate/get_phone_rate/?format=json
 
         Response::
 
@@ -101,8 +100,8 @@ class VoipRateResource(ModelResource):
         #queryset = VoIPRetailRate.objects.all()
         authorization = Authorization()
         authentication = BasicAuthentication()
-        allowed_methods = ['get']
-        detail_allowed_methods = ['get']
+        allowed_methods = ['get', 'post']
+        detail_allowed_methods = ['get', 'post']
         throttle = BaseThrottle(throttle_at=1000, timeframe=3600)  # default 1000 calls / hour
 
     def override_urls(self):
@@ -110,8 +109,6 @@ class VoipRateResource(ModelResource):
         return [
             url(r'^(?P<resource_name>%s)/$' % self._meta.resource_name,
                 self.wrap_view('read')),
-            url(r'^(?P<resource_name>%s)/get_phone_rate/$' % self._meta.resource_name,
-                self.wrap_view('create')),
             ]
 
     def read_response(self, request, data,
@@ -132,17 +129,45 @@ class VoipRateResource(ModelResource):
         logger.debug('Voip Rate GET API authorization called!')
         auth_result = self._meta.authorization.is_authorized(request, object)
 
-
         user_voip_plan = UserProfile.objects.get(user=request.user)
         voipplan_id = user_voip_plan.voipplan_id #  1
 
-        temp_url = request.META['PATH_INFO']
-        temp_id = temp_url.split('/api/v1/voip_rate/')[1]
-        code = temp_id.split('/')[0]
+        code = ''
+        if request.POST:
+            post_var = {}
+            for i in request.POST:
+                post_var = i
+                break
+
+            post_var = ast.literal_eval(post_var)
+
+            if post_var.get('recipient_phone_no'):
+                recipient_phone_no = int(post_var.get('recipient_phone_no'))
+                # Should Not banned recipient_phone_no
+                allowed = prefix_allowed_to_voip_call(recipient_phone_no, voipplan_id)
+                if allowed:
+                    # Get Destination prefix list e.g (34,346,3465,34657)
+                    destination_prefix_list = prefix_list_string(str(recipient_phone_no))
+
+                    # Split Destination prefix list
+                    list = destination_prefix_list.split(",")
+
+                    # Get Rate List
+                    rate_list = VoIPRetailRate.objects.values('prefix', 'retail_rate', 'prefix__destination').filter(prefix__in=[int(s) for s in list])
+
+                    logger.debug('Voip Rate API : result ok 200')
+                    return self.create_response(request, rate_list)
+                else:
+                    error_msg = "Not allowed : %s" % recipient_phone_no
+                    logger.error(error_msg)
+                    raise BadRequest(error_msg)
+
+            if post_var.get('code'):
+                code = int(post_var.get('code'))
 
         cursor = connection.cursor()
 
-        if code :
+        if code:
             try:
                 code = int(code)
             except ValueError:
@@ -210,55 +235,3 @@ class VoipRateResource(ModelResource):
 
         logger.debug('Voip Rate API : result ok 200')
         return self.read_response(request, result)
-
-
-    def create_response(self, request, data, response_class=HttpResponse,
-                        **response_kwargs):
-        desired_format = self.determine_format(request)
-        serialized = self.serialize(request, data, desired_format)
-        return response_class(content=serialized, content_type=desired_format,
-            **response_kwargs)
-
-    def create(self, request=None, **kwargs):
-        logger.debug('Voip Rate GET API get called')
-        auth_result = self._meta.authentication.is_authenticated(request)
-        if not auth_result is True:
-            raise ImmediateHttpResponse(response=http.HttpUnauthorized())
-
-        logger.debug('Voip Rate GET API authorization called!')
-        auth_result = self._meta.authorization.is_authorized(request, object)
-
-        j = 0
-        post_var = {}
-
-        for i in request.POST:
-            if j == 0:
-                post_var = i
-                j = j + 1
-
-        post_var = ast.literal_eval(post_var)
-        recipient_phone_no = int(post_var.get('recipient_phone_no'))
-
-        user_voip_plan = UserProfile.objects.get(user=request.user)
-        voipplan_id = user_voip_plan.voipplan_id #  1
-
-        # Should Not banned recipient_phone_no
-        allowed = prefix_allowed_to_voip_call(recipient_phone_no, voipplan_id)
-        if allowed:
-            # Get Destination prefix list e.g (34,346,3465,34657)
-            destination_prefix_list = prefix_list_string(str(recipient_phone_no))
-
-            # Split Destination prefix list
-            list = destination_prefix_list.split(",")
-
-            # Get Rate List
-            rate_list = VoIPRetailRate.objects.values('prefix', 'retail_rate', 'prefix__destination').filter(prefix__in=[int(s) for s in list])
-
-            logger.debug('Voip Rate API : result ok 200')
-            return self.create_response(request, rate_list)
-        else:
-            error_msg = "Not allowed : %s" % recipient_phone_no
-            logger.error(error_msg)
-            raise BadRequest(error_msg)
-
-
