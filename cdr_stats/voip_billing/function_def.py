22ff9d5a6e143deb6450837ff8633e6e686690b1
Areski Belaid <areski@gmail.com>
2013-01-16 18:40:05 +0100

    Change setting PHONENUMBER_PREFIX_LIMIT_* for PREFIX_LIMIT_*

--- cdr_stats/voip_billing/function_def.py
+++ cdr_stats/voip_billing/function_def.py
@@ -70,12 +70,12 @@ def prefix_list_string(phone_number):
     prefix_string = (34650, 3465, 346, 34)
     """
     phone_number = str(phone_number)
-    prefix_range = range(settings.PHONENUMBER_PREFIX_LIMIT_MIN,
-                         settings.PHONENUMBER_PREFIX_LIMIT_MAX + 1)
+    prefix_range = range(settings.PREFIX_LIMIT_MIN,
+                         settings.PREFIX_LIMIT_MAX + 1)
     prefix_range.reverse()
     destination_prefix_list = ''
     for i in prefix_range:
-        if i == settings.PHONENUMBER_PREFIX_LIMIT_MIN:
+        if i == settings.PREFIX_LIMIT_MIN:
             destination_prefix_list = destination_prefix_list + \
             phone_number[0:i]
         else:
--- install/conf/settings_local.py
+++ install/conf/settings_local.py
@@ -86,10 +86,10 @@ SOCKETIO_CALLNUM_DEFAULT = 0
 
 #GENERAL
 #=======
-# PHONENUMBER_PREFIX_LIMIT_MIN & PHONENUMBER_PREFIX_LIMIT_MAX are used to know
+# PREFIX_LIMIT_MIN & PREFIX_LIMIT_MAX are used to know
 # how many digits are used to match against the dialcode prefix database
-PHONENUMBER_PREFIX_LIMIT_MIN = 2
-PHONENUMBER_PREFIX_LIMIT_MAX = 5
+PREFIX_LIMIT_MIN = 2
+PREFIX_LIMIT_MAX = 5
 
 # If PN is lower than PN_MIN_DIGITS it will be considered as an extension
 # If PN is longer than PN_MIN_DIGITS but lower than PN_MAX_DIGITS then
