d2de369c879331b6d1258ece5219846c83c9df3a
Shrenik Patel <shrenik181986@gmail.com>
2013-01-16 17:22:20 +0530

    added constatns for voip_report

---
+++ cdr_stats/voip_report/constants.py
@@ -0,0 +1,24 @@
+#
+# CDR-Stats License
+# http://www.cdr-stats.org
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# Copyright (C) 2011-2012 Star2Billing S.L.
+#
+# The Initial Developer of the Original Code is
+# Arezqui Belaid <info@star2billing.com>
+#
+
+from django.utils.translation import ugettext as _
+from common.utils import Choice
+
+
+class BILLED_STATUS_LIST(Choice):
+    all = 'all', _('ALL')
+    yes = 'yes', _('YES')
+    no = 'no', _('NO')
+
+
