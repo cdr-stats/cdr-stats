2e498565b1fca9087c34bdc2cbb2ae007457d1e8
Shrenik Patel <shrenik181986@gmail.com>
2013-01-28 18:08:23 +0530

    Sample cdr report on admin side

--- cdr_stats/voip_billing/admin.py
+++ cdr_stats/voip_billing/admin.py
@@ -5,6 +5,9 @@ from django.template import RequestContext
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from django.conf import settings
+from pymongo.connection import Connection
+from pymongo.errors import ConnectionFailure
+
 from country_dialcode.models import Prefix
 from voip_billing.models import VoIPRetailRate, VoIPPlan, BanPlan,\
     VoIPPlan_BanPlan, BanPrefix, VoIPRetailPlan, VoIPPlan_VoIPRetailPlan,\
@@ -15,8 +18,22 @@ from voip_billing.forms import RetailRate_fileImport, CarrierRate_fileImport,\
 from voip_billing.widgets import AutocompleteModelAdmin
 from voip_billing.function_def import rate_filter_range_field_chk
 from voip_billing.rate_engine import rate_engine
-from common.common_functions import variable_value
+from common.common_functions import current_view, get_news, \
+    variable_value, mongodb_str_filter, mongodb_int_filter, \
+    int_convert_to_minute, validate_days, ceil_strdate
+
+from cdr.functions_def import get_country_name, \
+    chk_account_code, get_hangupcause_name
+from cdr.forms import CdrSearchForm
+from cdr.aggregate import pipeline_cdr_view_daily_report
+from cdr.constants import CDR_COLUMN_NAME
+from cdr.views import cdr_view_daily_report, unset_session_var, get_pagination_vars
+from datetime import datetime, date, timedelta
+from dateutil.relativedelta import relativedelta
 import csv
+import logging
+
+cdr_data = settings.DBCON[settings.MONGO_CDRSTATS['CDR_COMMON']]
 
 
 def export_as_csv_action(description="Export selected objects as CSV file",
@@ -130,8 +147,340 @@ class VoIPPlanAdmin(admin.ModelAdmin):
             (r'^/(.+)/$', self.admin_site.admin_view(self.change_view)),
             (r'^simulator/$', self.admin_site.admin_view(self.simulator)),
             (r'^export/$', self.admin_site.admin_view(self.export)),
+            (r'^cdr_view/$', self.admin_site.admin_view(self.cdr_view)),
         )
         return my_urls + urls
+
+
+    def cdr_view(self, request):
+        """List of CDRs
+
+        **Attributes**:
+
+            * ``template`` - admin/voip_billing/voipplan/cdr_view.html
+            * ``form`` - CdrSearchForm
+            * ``mongodb_data_set`` - MONGO_CDRSTATS['CDR_COMMON']
+
+        **Logic Description**:
+
+            get the call records as well as daily call analytics
+            from mongodb collection according to search parameters
+        """
+        template_name = 'admin/voip_billing/voipplan/cdr_view.html'
+        opts = VoIPPlan._meta
+        logging.debug('CDR View Start')
+        query_var = {}
+        result = 1  # default min
+        switch_id = 0  # default all
+        hangup_cause_id = 0  # default all
+        destination = ''
+        destination_type = ''
+        dst = ''
+        accountcode = ''
+        accountcode_type = ''
+        acc = ''
+        direction = ''
+        duration = ''
+        duration_type = ''
+        due = ''
+        caller = ''
+        caller_type = ''
+        cli = ''
+        search_tag = 0
+        action = 'tabs-1'
+        menu = 'on'
+        cdr_view_daily_data = {}
+        if request.method == 'POST':
+            logging.debug('CDR Search View')
+            search_tag = 1
+            request.session['session_search_tag'] = search_tag
+            form = CdrSearchForm(request.POST)
+            if form.is_valid():
+                # set session var value
+                field_list = ['destination', 'result', 'destination_type', 'accountcode',
+                              'accountcode_type', 'caller', 'caller_type', 'duration',
+                              'duration_type', 'hangup_cause_id', 'switch_id', 'direction',
+                              'country_id']
+                unset_session_var(request, field_list)
+
+                request.session['session_cdr_view_daily_data'] = {}
+
+                if "from_date" in request.POST:
+                    # From
+                    from_date = request.POST['from_date']
+                    start_date = ceil_strdate(from_date, 'start')
+                    request.session['session_from_date'] = from_date
+
+                if "to_date" in request.POST:
+                    # To
+                    to_date = request.POST['to_date']
+                    end_date = ceil_strdate(to_date, 'end')
+                    request.session['session_to_date'] = to_date
+
+                result = request.POST['result']
+                if result:
+                    request.session['session_result'] = int(result)
+
+                destination = variable_value(request, 'destination')
+                destination_type = variable_value(request, 'destination_type')
+                if destination:
+                    request.session['session_destination'] = destination
+                    request.session['session_destination_type'] = destination_type
+
+                accountcode = variable_value(request, 'accountcode')
+                accountcode_type = variable_value(request, 'accountcode_type')
+                if accountcode:
+                    request.session['session_accountcode'] = accountcode
+                    request.session['session_accountcode_type'] = accountcode_type
+
+                caller = variable_value(request, 'caller')
+                caller_type = variable_value(request, 'caller_type')
+                if caller:
+                    request.session['session_caller'] = caller
+                    request.session['session_accountcode_type'] = caller_type
+
+                duration = variable_value(request, 'duration')
+                duration_type = variable_value(request, 'duration_type')
+                if duration:
+                    request.session['session_duration'] = duration
+                    request.session['session_duration_type'] = duration_type
+
+                direction = variable_value(request, 'direction')
+                if direction and direction != 'all':
+                    request.session['session_direction'] = str(direction)
+
+                switch_id = variable_value(request, 'switch')
+                if switch_id:
+                    request.session['session_switch_id'] = switch_id
+
+                hangup_cause_id = variable_value(request, 'hangup_cause')
+                if hangup_cause_id:
+                    request.session['session_hangup_cause_id'] = hangup_cause_id
+
+                records_per_page = variable_value(request, 'records_per_page')
+                if records_per_page:
+                    request.session['session_records_per_page'] = records_per_page
+
+                country_id = form.cleaned_data.get('country_id')
+                # convert list value in int
+                country_id = [int(row) for row in country_id]
+                if len(country_id) >= 1:
+                    request.session['session_country_id'] = country_id
+            else:
+                # form is not valid
+                logging.debug('Error : CDR search form')
+                tday = datetime.today()
+                start_date = datetime(tday.year, tday.month, 1)
+                last_day = ((datetime(tday.year, tday.month, 1, 23, 59, 59, 999999)
+                    + relativedelta(months=1))
+                    - relativedelta(days=1)).strftime('%d')
+                end_date = datetime(tday.year, tday.month, int(last_day))
+                cdr_view_daily_data = {
+                    'total_data': [],
+                    'total_duration': 0,
+                    'total_calls': 0,
+                    'total_avg_duration': 0,
+                    'max_duration': 0,
+                }
+                template_data = RequestContext(request, {
+                    'module': current_view(request),
+                    'rows': [],
+                    'form': form,
+                    'PAGE_SIZE': settings.PAGE_SIZE,
+                    'cdr_daily_data': cdr_view_daily_data,
+                    'search_tag': search_tag,
+                    'col_name_with_order': [],
+                    'menu': menu,
+                    'start_date': start_date,
+                    'end_date': end_date,
+                    'action': action,
+                    'result': result,                    
+                    'CDR_COLUMN_NAME': CDR_COLUMN_NAME,
+                    'opts': opts,
+                    'model_name': opts.object_name.lower(),
+                    'app_label': _('VoIP Billing'),
+                })
+                logging.debug('CDR View End')
+                return render_to_response(template_name, context_instance=template_data)
+
+        menu = 'off'
+
+        try:
+            if request.GET.get('page') or request.GET.get('sort_by'):
+                from_date = request.session.get('session_from_date')
+                to_date = request.session.get('session_to_date')
+                destination = request.session.get('session_destination')
+                destination_type = request.session.get('session_destination_type')
+                accountcode = request.session.get('session_accountcode')
+                accountcode_type = request.session.get('session_accountcode_type')
+                caller = request.session.get('session_caller')
+                caller_type = request.session.get('session_caller_type')
+                duration = request.session.get('session_duration')
+                duration_type = request.session.get('session_duration_type')
+                direction = request.session.get('session_direction')
+                switch_id = request.session.get('session_switch_id')
+                hangup_cause_id = request.session.get('session_hangup_cause_id')
+                result = int(request.session.get('session_result'))
+                search_tag = request.session.get('session_search_tag')
+                records_per_page = request.session.get('session_records_per_page')
+                country_id = request.session['session_country_id']
+                cdr_view_daily_data = request.session.get('session_cdr_view_daily_data')
+            else:
+                from_date
+        except NameError:
+            tday = datetime.today()
+            from_date = tday.strftime('%Y-%m-01')
+            last_day = ((datetime(tday.year, tday.month, 1, 23, 59, 59, 999999) +
+                        relativedelta(months=1)) -
+                        relativedelta(days=1)).strftime('%d')
+            to_date = tday.strftime('%Y-%m-' + last_day)
+            search_tag = 0
+            country_id = ''
+            records_per_page = settings.PAGE_SIZE
+            # unset session var value
+            request.session['session_result'] = 1
+            request.session['session_from_date'] = from_date
+            request.session['session_to_date'] = to_date
+
+            field_list = ['destination', 'destination_type', 'accountcode',
+                          'accountcode_type', 'caller', 'caller_type', 'duration',
+                          'duration_type', 'hangup_cause_id', 'switch_id', 'direction',
+                          'country_id']
+            unset_session_var(request, field_list)
+
+            request.session['session_search_tag'] = search_tag
+            request.session['session_records_per_page'] = records_per_page
+            request.session['session_country_id'] = ''
+            request.session['session_cdr_view_daily_data'] = {}
+
+        start_date = ceil_strdate(from_date, 'start')
+        end_date = ceil_strdate(to_date, 'end')
+
+        query_var['start_uepoch'] = {'$gte': start_date, '$lt': end_date}
+
+        # aggregate query variable
+        daily_report_query_var = {}
+        daily_report_query_var['metadata.date'] = {'$gte': start_date, '$lt': end_date}
+
+        dst = mongodb_str_filter(destination, destination_type)
+        if dst:
+            query_var['destination_number'] = dst
+
+        if request.user.is_superuser:
+            # superuser can see everything
+            acc = mongodb_str_filter(accountcode, accountcode_type)
+            if acc:
+                daily_report_query_var['metadata.accountcode'] = acc
+                query_var['accountcode'] = acc
+
+        if not request.user.is_superuser:
+            daily_report_query_var['metadata.accountcode'] = chk_account_code(request)
+            query_var['accountcode'] = daily_report_query_var['metadata.accountcode']
+
+        cli = mongodb_str_filter(caller, caller_type)
+        if cli:
+            query_var['caller_id_number'] = cli
+
+        due = mongodb_int_filter(duration, duration_type)
+        if due:
+            query_var['duration'] = daily_report_query_var['duration_daily'] = due
+
+        if switch_id and int(switch_id) != 0:
+            daily_report_query_var['metadata.switch_id'] = int(switch_id)
+            query_var['switch_id'] = int(switch_id)
+
+        if hangup_cause_id and int(hangup_cause_id) != 0:
+            daily_report_query_var['metadata.hangup_cause_id'] = int(hangup_cause_id)
+            query_var['hangup_cause_id'] = int(hangup_cause_id)
+
+        if direction and direction != 'all':
+            query_var['direction'] = str(direction)
+
+        if len(country_id) >= 1 and country_id[0] != 0:
+            daily_report_query_var['metadata.country_id'] = {'$in': country_id}
+            query_var['country_id'] = {'$in': country_id}
+
+        final_result = cdr_data.find(query_var,
+            {
+                "uuid": 0,
+                "answer_uepoch": 0,
+                "end_uepoch": 0,
+                "mduration": 0,
+                "billmsec": 0,
+                "read_codec": 0,
+                "write_codec": 0,
+                "remote_media_ip": 0,
+            }
+        )
+
+        form = CdrSearchForm(
+            initial={
+                'from_date': from_date,
+                'to_date': to_date,
+                'destination': destination,
+                'destination_type': destination_type,
+                'accountcode': accountcode,
+                'accountcode_type': accountcode_type,
+                'caller': caller,
+                'caller_type': caller_type,
+                'duration': duration,
+                'duration_type': duration_type,
+                'result': result,
+                'direction': direction,
+                'hangup_cause': hangup_cause_id,
+                'switch': switch_id,
+                'country_id': country_id,
+                'records_per_page': records_per_page
+            }
+        )
+
+        request.session['query_var'] = query_var
+
+        # Define no of records per page
+        PAGE_SIZE = int(records_per_page)
+        pagination_data = get_pagination_vars(request)
+
+        PAGE_NUMBER = pagination_data['PAGE_NUMBER']
+        col_name_with_order = pagination_data['col_name_with_order']
+        sort_field = pagination_data['sort_field']
+        default_order = pagination_data['default_order']
+
+        logging.debug('Create cdr result')
+        SKIP_NO = PAGE_SIZE * (PAGE_NUMBER - 1)
+        record_count = final_result.count()
+        rows = final_result.skip(SKIP_NO).limit(PAGE_SIZE)\
+            .sort([(sort_field, default_order)])
+
+        # Get daily report from session while using pagination & sorting
+        if request.GET.get('page') or request.GET.get('sort_by'):
+            cdr_view_daily_data = request.session['session_cdr_view_daily_data']
+        else:
+            # pass aggregate query to cdr_view_daily_report
+            cdr_view_daily_data = cdr_view_daily_report(daily_report_query_var)
+            request.session['session_cdr_view_daily_data'] = cdr_view_daily_data
+
+        template_data = RequestContext(request, {
+            'module': current_view(request),
+            'rows': rows,
+            'form': form,
+            'record_count': record_count,
+            'PAGE_SIZE': PAGE_SIZE,
+            'cdr_daily_data': cdr_view_daily_data,
+            'search_tag': search_tag,
+            'col_name_with_order': col_name_with_order,
+            'menu': menu,
+            'start_date': start_date,
+            'end_date': end_date,
+            'action': action,
+            'result': int(result),            
+            'CDR_COLUMN_NAME': CDR_COLUMN_NAME,
+            'opts': opts,
+            'model_name': opts.object_name.lower(),
+            'app_label': _('VoIP Billing'),
+        })
+        logging.debug('CDR View End')            
+        return render_to_response(template_name, context_instance=template_data)        
+
 
     def changelist_view(self, request, extra_context=None):
         """
