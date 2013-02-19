002c9f0376798ec6341e7cb80a4bd0e83e899c71
Areski Belaid <areski@gmail.com>
2013-02-19 13:57:03 +0100

    update logger to only run on deployed version / update req django-frontend-notification==0.1.4

--- install/conf/settings_local.py
+++ install/conf/settings_local.py
@@ -169,3 +169,72 @@ MONGO_CDRSTATS = {
     'CONC_CALL': 'concurrent_call',
     'CONC_CALL_AGG': 'concurrent_call_aggregate'
 }
+
+#LOGGING
+#=======
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'root': {
+        'level': 'DEBUG',
+        'handlers': ['default'],
+    },
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+        },
+        'simple': {
+            'format': '%(asctime)s %(levelname)s || %(message)s'
+        },
+    },
+    'handlers': {
+        # Include the default Django email handler for errors
+        # This is what you'd get without configuring logging at all.
+        'mail_admins': {
+            'class': 'django.utils.log.AdminEmailHandler',
+            'level': 'ERROR',
+            'include_html': True,
+        },
+        'default': {
+            'class': 'logging.handlers.WatchedFileHandler',
+            'filename': '/var/log/cdr-stats/cdr-stats.log',
+            'formatter': 'verbose',
+        },
+        'default-db': {
+            'level': 'DEBUG',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': '/var/log/cdr-stats/cdr-stats-db.log',
+            'maxBytes': 1024 * 1024 * 5,  # 5 MB
+            'backupCount': 20,
+            'formatter': 'verbose',
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        },
+    },
+    'loggers': {
+        # Again, default Django configuration to email unhandled exceptions
+        'django': {
+            'handlers': ['default'],
+            'propagate': False,
+            'level': 'DEBUG',
+        },
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+        'cdr-stats.filelog': {
+            'handlers': ['default'],
+            'level': 'DEBUG',
+            'propagate': False,
+        },
+        'django.db.backends': {
+            'handlers': ['default-db'],
+            'level': 'DEBUG',
+            'propagate': False,
+        },
+    },
+}
