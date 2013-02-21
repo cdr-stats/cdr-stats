e927d3bc0b48e3f114bd2bb5766d2d79aafa7406
Shrenik Patel <shrenik181986@gmail.com>
2013-02-21 11:33:33 +0530

    refactor voip_billing admin.py

--- cdr_stats/voip_billing/admin.py
+++ cdr_stats/voip_billing/admin.py
@@ -20,8 +20,6 @@ from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from django.conf import settings
 from django.contrib import messages
-from django.db.models.base import ModelBase
-from django.core.urlresolvers import resolve
 
 from user_profile.models import UserProfile
 from country_dialcode.models import Prefix
@@ -38,74 +36,13 @@ from voip_billing.function_def import rate_filter_range_field_chk
 from voip_billing.rate_engine import rate_engine
 from voip_billing.tasks import RebillingTask, ReaggregateTask
 from common.common_functions import variable_value, ceil_strdate
+from common.app_label_renamer import AppLabelRenamer
 from mongodb_connection import mongodb
 from datetime import datetime
 import csv
 
 APP_LABEL = _('VoIP Billing')
-
-class AppLabelRenamer(object):
-    ''' Rename app label and app breadcrumbs in admin. '''
-    def __init__(self, native_app_label, app_label):
-        self.native_app_label = native_app_label
-        self.app_label = app_label
-        self.module = '.'.join([native_app_label, 'models'])
-
-    class string_with_realoaded_title(str):
-        ''' tnx to Ionel Maries Cristian for http://ionelmc.wordpress.com/2011/06/24/custom-app-names-in-the-django-admin/'''
-        def __new__(cls, value, title):
-            instance = str.__new__(cls, value)
-            instance._title = title
-            return instance
-
-        def title(self):
-            return self._title
-
-        __copy__ = lambda self: self
-        __deepcopy__ = lambda self, memodict: self
-
-    def rename_app_label(self, f):
-        app_label = self.app_label
-
-        def rename_breadcrumbs(f):
-            def wrap(self, *args, **kwargs):
-                extra_context = kwargs.get('extra_context', {})
-                extra_context['app_label'] = app_label
-                kwargs['extra_context'] = extra_context
-                return f(self, *args, **kwargs)
-            return wrap
-
-        def wrap(model_or_iterable, admin_class=None, **option):
-            if isinstance(model_or_iterable, ModelBase):
-                model_or_iterable = [model_or_iterable]
-            for model in model_or_iterable:
-                if model.__module__ != self.module:
-                    continue
-                if admin_class is None:
-                    admin_class = type(model.__name__+'Admin', (admin.ModelAdmin,), {})
-                admin_class.add_view = rename_breadcrumbs(admin_class.add_view)
-                admin_class.change_view = rename_breadcrumbs(admin_class.change_view)
-                admin_class.changelist_view = rename_breadcrumbs(admin_class.changelist_view)
-                model._meta.app_label = self.string_with_realoaded_title(self.native_app_label, self.app_label)
-            return f(model, admin_class, **option)
-        return wrap
-
-    def rename_app_index(self, f):
-        def wrap(request, app_label, extra_context=None):
-            requested_app_label = resolve(request.path).kwargs.get('app_label', '')
-            if requested_app_label and requested_app_label == self.native_app_label:
-                app_label = self.string_with_realoaded_title(self.native_app_label, self.app_label)
-            else:
-                app_label = requested_app_label
-            return f(request, app_label, extra_context=None)
-        return wrap
-
-    def main(self):
-        admin.site.register = self.rename_app_label(admin.site.register)
-        admin.site.app_index = self.rename_app_index(admin.site.app_index)
-
 AppLabelRenamer(native_app_label=u'voip_billing', app_label=u'Voip Billing').main()
-
 
 
 def export_as_csv_action(description="Export selected objects as CSV file",
