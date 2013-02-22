0d159fb563a66ea5706a4c781b65570923c20b63
Shrenik Patel <shrenik181986@gmail.com>
2013-02-22 19:17:51 +0530

    refactor voip_billing admin.py

--- cdr_stats/voip_billing/admin.py
+++ cdr_stats/voip_billing/admin.py
@@ -37,48 +37,13 @@ from voip_billing.rate_engine import rate_engine
 from voip_billing.tasks import RebillingTask, ReaggregateTask
 from common.common_functions import variable_value, ceil_strdate
 from common.app_label_renamer import AppLabelRenamer
+from common.admin_custom_actions import export_as_csv_action
 from mongodb_connection import mongodb
 from datetime import datetime
 import csv
 
 APP_LABEL = _('VoIP Billing')
 AppLabelRenamer(native_app_label=u'voip_billing', app_label=_('Voip Billing')).main()
-
-
-def export_as_csv_action(description="Export selected objects as CSV file",
-                         fields=None, exclude=None, header=True,):
-    """
-    This function returns an export csv action
-    'fields' and 'exclude' work like in django ModelForm
-    'header' is whether or not to output the column names as the first row
-    """
-    def export_as_csv(modeladmin, request, queryset):
-        """
-        Generic csv export admin action.
-        based on http://djangosnippets.org/snippets/1697/
-        """
-        opts = modeladmin.model._meta
-        field_names = set([field.name for field in opts.fields])
-
-        if fields:
-            fieldset = set(fields)
-            field_names = field_names & fieldset
-        elif exclude:
-            excludeset = set(exclude)
-            field_names = field_names - excludeset
-
-        response = HttpResponse(mimetype='text/csv')
-        response['Content-Disposition'] = 'attachment; filename=%s.csv' % \
-            unicode(opts).replace('.', '_')
-
-        writer = csv.writer(response)
-        if header:
-            writer.writerow(field_names)
-        for obj in queryset:
-            writer.writerow([unicode(getattr(obj, field)) for field in field_names])
-        return response
-    export_as_csv.short_description = description
-    return export_as_csv
 
 
 def prefix_qs():
