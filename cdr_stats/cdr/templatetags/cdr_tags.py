56012a5f95d3ff1b0d938f6280a258be59f66d43
Shrenik Patel <shrenik181986@gmail.com>
2013-03-05 16:36:09 +0530

    move cdr_alert template tags into separate dir

--- cdr_stats/cdr/templatetags/cdr_tags.py
+++ cdr_stats/cdr/templatetags/cdr_tags.py
@@ -14,8 +14,6 @@
 from django import template
 from cdr.models import Switch
 from cdr.functions_def import get_hangupcause_name
-from cdr_alert.constants import PERIOD, ALARM_TYPE, STATUS,\
-    ALARM_REPROT_STATUS, ALERT_CONDITION
 import re
 
 register = template.Library()
@@ -65,89 +63,6 @@ def mongo_id(value, sub_val):
         else:
             value = value['_id']
     return value
-
-
-@register.filter(name='alarm_period')
-def alarm_period(value):
-    """alarm period
-
-    >>> alarm_period(1)
-    'START'
-    """
-    if not value:
-        return ''
-
-    period = dict(PERIOD)
-    try:
-        period = period[value]
-    except:
-        period = ''
-
-    return str(period)
-
-
-@register.filter(name='alarm_type')
-def alarm_type(value):
-    """alarm type
-
-    >>> alarm_type(1)
-    'START'
-    """
-    if not value:
-        return ''
-
-    alarm_type = dict(ALARM_TYPE)
-    try:
-        alarm_type = alarm_type[value]
-    except:
-        alarm_type = ''
-
-    return str(alarm_type)
-
-
-@register.filter(name='alarm_status')
-def alarm_status(value):
-    """alarm status
-    """
-    if not value:
-        return ''
-    status = dict(STATUS)
-    try:
-        status = status[value]
-    except:
-        status = ''
-
-    return str(status)
-
-
-@register.filter(name='alarm_condition')
-def alarm_condition(value):
-    """alarm report status
-    """
-    if not value:
-        return ''
-    alarm_condition = dict(ALERT_CONDITION)
-    try:
-        alarm_condition = alarm_condition[value]
-    except:
-        alarm_condition = ''
-
-    return str(alarm_condition)
-
-
-@register.filter(name='alarm_report_status')
-def alarm_report_status(value):
-    """alarm report status
-    """
-    if not value:
-        return ''
-    STATUS = dict(ALARM_REPROT_STATUS)
-    try:
-        status = STATUS[value]
-    except:
-        status = ''
-
-    return str(status)
 
 
 @register.filter(name='get_cost')
