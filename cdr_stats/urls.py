49c6e0fc5e407e1346619e6ae7b2b38e8062fe70
Shrenik Patel <shrenik181986@gmail.com>
2013-03-13 19:26:35 +0530

    update urls.py

--- cdr_stats/urls.py
+++ cdr_stats/urls.py
@@ -65,9 +65,6 @@ urlpatterns = patterns('',
     (r'^static/(?P<path>.*)$', 'django.views.static.serve',
         {'document_root': settings.STATIC_ROOT}),
 
-    #(r'^favicon\.ico$', 'django.views.generic.simple.redirect_to',
-    #    {'url': 'static/cdr_stats/images/favicon.ico'}),
-
     (r'^%s/' % settings.DAJAXICE_MEDIA_PREFIX, include('dajaxice.urls')),
 )
 
