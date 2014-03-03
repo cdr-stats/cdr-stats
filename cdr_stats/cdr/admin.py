d38a4258287752ef5569292f4291d0f536eaacfc
shrenik <shrenik181986@gmail.com>
2014-03-03 13:27:24 +0530

    update diagnostic template

--- cdr_stats/cdr/admin.py
+++ cdr_stats/cdr/admin.py
@@ -39,7 +39,7 @@ from mongodb_connection import mongodb
 from django_lets_go.common_functions import getvar, unset_session_var
 from django_lets_go.app_label_renamer import AppLabelRenamer
 APP_LABEL = _('CDR')
-AppLabelRenamer(native_app_label=u'cdr', app_label=APP_LABEL).main()
+AppLabelRenamer(native_app_label='cdr', app_label=APP_LABEL).main()
 
 
 def get_value_from_uni(j, row, field_name):
