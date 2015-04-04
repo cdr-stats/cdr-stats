2ed1a1899c4faef6e721644a3901e8f51809f136
areski <areski@gmail.com>
2015-04-05 01:16:30 +0200

    update copyright date

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -55,7 +55,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'CDR-Stats'
-copyright = u'2011-2013, Arezqui Belaid <info@star2billing.com>'
+copyright = u'2011-2015, Arezqui Belaid <info@star2billing.com>'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
